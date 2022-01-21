068c11097a3ede4aa4187a6f38e499ef9830c917
z1youRA <374671110@qq.com>
2022-01-21 16:27:00 +0800

    get add panel to work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createContainer} from './main';
 import { createHeader } from './header.js';
 import { createSidebar } from './sidebar.js';
 
-createContainer.addTodo();
+createContainer.createAddButton();
+createContainer.addTodo('abc', 'aaa', '2022-01-24', '1');
 createHeader();
 createSidebar();
--- src/main.js
+++ src/main.js
@@ -4,12 +4,66 @@ import Unstar from './img/unstar.png';
 // Create the main part of the page.
 const createContainer = (() => {
     const container = document.querySelector('.container');
+
     // Setup a todo logically, attribute information to it.
     const createTodo = (title, description, dueDate, importance, status) => {
         return {title, description, dueDate, importance, status};
     }
-    
-    
+
+    const _clear = (block) => {
+        block.textContent = '';
+    }
+
+    // Display the collapsed add button.
+    const displayAddButton = () => {
+        const addButton = document.createElement('div');
+        addButton.classList.add('add-button');
+        addButton.classList.add('collapse');
+        const icon = document.createElement('div');
+        icon.classList.add('add-icon');
+        const text = document.createElement('div');
+
+        icon.textContent = '+';
+        text.textContent = 'Add a task';
+        addButton.appendChild(icon);
+        addButton.appendChild(text);
+        container.appendChild(addButton);
+    }
+
+    // Expand add panel after clicking.
+    const createAddButton = () => {
+        displayAddButton();
+        const addButton = document.querySelector('.add-button');
+        addButton.addEventListener('click', expandAddButton);
+    }
+
+    const expandAddButton = () => {
+        const addButton = document.querySelector('.add-button');
+        addButton.classList.remove('collapse');
+        addButton.classList.add('expand');
+        _clear(addButton);
+        const addStatus = document.createElement('input');
+        const addTitle = document.createElement('input');
+        const addDueDate = document.createElement('input');
+        const addTrigger = document.createElement('div');
+
+        addTrigger.classList.add('add-trigger');
+        addTrigger.textContent = 'ADD';
+        addTrigger.addEventListener('click', () => {
+            addTodo(addTitle.value, '', addDueDate.valueAsDate, '0');
+        });
+        addStatus.classList.add('round-checkbox', 'add-status');
+        addStatus.setAttribute('type', 'checkbox');
+        addTitle.classList.add('add-title');
+        addDueDate.setAttribute('type', 'date');
+
+        addButton.removeEventListener('click', expandAddButton);
+        addButton.appendChild(addStatus);
+        addButton.appendChild(addTitle);
+        addButton.appendChild(addDueDate);
+        addButton.appendChild(addTrigger);
+    }
+
     // Create elements of todo on the page.
     const displayCollapseTodo = (todo) => {
         const todoCollapse = document.createElement('div');
@@ -17,7 +71,7 @@ const createContainer = (() => {
         const todoStatus = document.createElement('input');
         const todoTitle = document.createElement('div');
         const todoImportance = document.createElement('img');
-        todoStatus.classList.add('todo-status');
+        todoStatus.classList.add('todo-status', 'checkbox');
         todoStatus.setAttribute('type', 'checkbox');
         todoTitle.classList.add('todo-title');
         todoTitle.textContent = todo.title;
@@ -36,15 +90,16 @@ const createContainer = (() => {
         container.appendChild(todoCollapse);
     }
 
-    // const displayExpandTodo = (todo) => {
-
-    // }
-
-    const addTodo = () => {
-        let todoTemp = createTodo('Have lunch', 'have a lunch at noon', '2022-01-20', '1')
+    const addTodo = (title, description, dueDate, importance, status) => {
+        if(title == '' || title === undefined) {
+            alert("ERROR: Empty Title!");
+            return;
+        }
+        let todoTemp = createTodo(title, description, dueDate, importance, status)
         displayCollapseTodo(todoTemp);
     }
-    return {addTodo};
+
+    return {addTodo, createAddButton};
 })();
 
 // const todoDescription = document.createElement('div');
